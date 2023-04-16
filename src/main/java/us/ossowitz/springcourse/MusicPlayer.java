916ea43cd640b44f99072c1317a2912e9df64214
Ossowitz <76myxomor76@gmail.com>
2023-04-16 17:29:26 +0300

    InitialCommit

--- src/main/java/us/ossowitz/springcourse/MusicPlayer.java
+++ src/main/java/us/ossowitz/springcourse/MusicPlayer.java
@@ -1,21 +1,20 @@
 package us.ossowitz.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public List<Music> getMusicList() {
+        return musicList;
     }
 
-    public MusicPlayer() {
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public String getName() {
@@ -35,6 +34,8 @@ public class MusicPlayer {
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
