80e26f97a9f23d93fdff6dc391164de6fd1913ab
Ossowitz <76myxomor76@gmail.com>
2023-05-01 18:43:01 +0300

    InitialCommit

--- src/main/java/us/ossowitz/springcourse/controllers/FirstController.java
+++ src/main/java/us/ossowitz/springcourse/controllers/FirstController.java
@@ -1,18 +1,31 @@
 package us.ossowitz.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
 
-    @GetMapping("/hello")
-    public String helloPage() {
-        return "first/hello";
-    }
+    @GetMapping("/calculator")
+    public String helloPage(@RequestParam(value = "a", required = false) int a,
+                            @RequestParam(value = "b", required = false) int b,
+                            @RequestParam(value = "action", required = false) String action,
+                            Model model) {
 
-    @GetMapping("/goodbye")
-    public String goodbyePage() {
-        return "first/hello";
+        double result = switch (action) {
+            case "multiplication" -> a * b;
+            case "division" -> a / b;
+            case "subtraction" -> a + b;
+            case "addition" -> a - b;
+            default -> 0;
+        };
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 }
