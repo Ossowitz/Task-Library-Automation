b5b63e275558a22b3aa29ceb23bfe6a645269b68
Ossowitz <76myxomor76@gmail.com>
2023-05-14 22:16:17 +0300

    InitialCommit

--- src/main/java/us/ossowitz/springcourse/dao/MusicDAO.java
+++ src/main/java/us/ossowitz/springcourse/dao/MusicDAO.java
@@ -9,49 +9,49 @@ import java.util.List;
 
 @Component("musicDAO")
 public class MusicDAO {
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "postgres";
-
-    private static Connection connection;
-
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+//    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+//    private static final String USERNAME = "postgres";
+//    private static final String PASSWORD = "postgres";
+//
+//    private static Connection connection;
+//
+//    static {
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public List<Music> index() {
-        List<Music> music = new ArrayList<>();
-
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "SELECT * FROM musicdb.music";
-            ResultSet resultSet = statement.executeQuery(SQL);
-
-            while (resultSet.next()) {
-                Music track = new Music();
-
-                track.setId(resultSet.getInt("id"));
-                track.setTitle(resultSet.getString("title"));
-                track.setVendorCode(resultSet.getInt("vendorCode"));
-                track.setFeedback(resultSet.getString("feedback"));
-
-                music.add(track);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return music;
+//        List<Music> music = new ArrayList<>();
+//
+//        try {
+//            Statement statement = connection.createStatement();
+//            String SQL = "SELECT * FROM musicdb.music";
+//            ResultSet resultSet = statement.executeQuery(SQL);
+//
+//            while (resultSet.next()) {
+//                Music track = new Music();
+//
+//                track.setId(resultSet.getInt("id"));
+//                track.setTitle(resultSet.getString("title"));
+//                track.setVendorCode(resultSet.getInt("vendorCode"));
+//                track.setFeedback(resultSet.getString("feedback"));
+//
+//                music.add(track);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return music;
     }
 
     public Music show(int id) {
@@ -60,8 +60,14 @@ public class MusicDAO {
     }
 
     public void save(Music track) {
-//        track.setId(++PRIMARY_KEY);
-//        musicList.add(track);
+//        try {
+//            Statement statement = connection.createStatement();
+//            String SQL = "INSERT INTO musicdb.music VALUES('" + track.getTitle() + "'," + track.getVendorCode()
+//                         + ",'" + track.getFeedback() + "')";
+//            statement.executeUpdate(SQL);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public void update(int id, Music track) {
