d19d56821cb7371af5329d98b503cf0b4405c72c
Ossowitz <76myxomor76@gmail.com>
2023-05-15 18:10:02 +0300

    InitialCommitOnBranch1

---
+++ src/main/java/us/ossowitz/springcourse/models/Children.java
@@ -0,0 +1,60 @@
+package us.ossowitz.springcourse.models;
+
+public class Children {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+
+    public Children() {
+    }
+
+    public Children(int id, String name, String surname, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
