e4d2e2c5944e052b22bc7a670f1138a31ff5e08c
Ossowitz <76myxomor76@gmail.com>
2023-05-16 01:00:37 +0300

    InitialCommit

--- src/main/java/us/ossowitz/springcourse/dao/MusicDAO.java
+++ src/main/java/us/ossowitz/springcourse/dao/MusicDAO.java
@@ -55,8 +55,29 @@ public class MusicDAO {
     }
 
     public Music show(int id) {
-        return null;
-//        return musicList.stream().filter(track -> id == track.getId()).findAny().orElse(null);
+        Music track = null;
+        String SQL = """
+                SELECT *
+                FROM musicdb.music
+                WHERE id = ?
+                """;
+        try {
+            var preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setInt(1, id);
+
+            var resultSet = preparedStatement.executeQuery();
+
+            resultSet.next();
+            track = new Music();
+
+            track.setId(resultSet.getInt("id"));
+            track.setTitle(resultSet.getString("title"));
+            track.setVendorCode(resultSet.getInt("vendorCode"));
+            track.setFeedback(resultSet.getString("feedback"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return track;
     }
 
     public void save(Music track) {
@@ -78,15 +99,37 @@ public class MusicDAO {
     }
 
     public void update(int id, Music track) {
-        Music trackToBeUpdated = show(id);
+        String SQL = """
+                UPDATE musicdb.music
+                SET title = ?, vendorcode = ?, feedback = ?
+                WHERE id = ?
+                """;
+        try {
+            var preparedStatement = connection.prepareStatement(SQL);
 
-        trackToBeUpdated.setTitle(track.getTitle());
-        trackToBeUpdated.setVendorCode(track.getVendorCode());
-        trackToBeUpdated.setFeedback(track.getFeedback());
+            preparedStatement.setString(1, track.getTitle());
+            preparedStatement.setInt(2, track.getVendorCode());
+            preparedStatement.setString(3, track.getFeedback());
+            preparedStatement.setInt(4, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void delete(int id) {
-//        musicList.removeIf(track -> id == track.getId());
+        String SQL = """
+                DELETE 
+                FROM musicdb.music
+                WHERE id = ?
+                """;
+        try {
+            var preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
 }
