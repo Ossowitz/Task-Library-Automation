3f6cb091018f04d4eb94effd39fb4c3a86f0dbae
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-05-30 09:26:40 +0300

    Add validator for «Phone number»

---
+++ src/main/java/util/person/phoneValidator/ContactNumberValidator.java
@@ -0,0 +1,20 @@
+package util.person.phoneValidator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ContactNumberValidator implements
+        ConstraintValidator<ContactNumberConstraint, String> {
+
+    @Override
+    public void initialize(ContactNumberConstraint contactNumber) {
+    }
+
+    @Override
+    public boolean isValid(String contactField,
+                           ConstraintValidatorContext cxt) {
+        return contactField != null && contactField.matches("[0-9]+")
+               && (contactField.length() == 11);
+    }
+
+}
