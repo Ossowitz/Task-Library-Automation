f2c63cd0835e1918fc195fa963dc74d83d9eaecb
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-05-30 18:14:24 +0300

    Implementing All People List Header Page

--- src/main/java/us/ossowitz/dao/PersonDAO.java
+++ src/main/java/us/ossowitz/dao/PersonDAO.java
@@ -1,8 +1,12 @@
 package us.ossowitz.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import us.ossowitz.models.person.Person;
+
+import java.util.List;
 
 @Component
 public class PersonDAO {
@@ -13,5 +17,8 @@ public class PersonDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM library_automation.person",
+                new BeanPropertyRowMapper<>(Person.class));
+    }
 }
