913f2807fb17102313f88062caffb8d8aed3c2be
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-06-01 22:17:44 +0300

    Adding constrains for «Book»

--- src/main/java/us/ossowitz/controllers/BooksController.java
+++ src/main/java/us/ossowitz/controllers/BooksController.java
@@ -2,6 +2,8 @@ package us.ossowitz.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import us.ossowitz.dao.BookDAO;
 
@@ -15,5 +17,9 @@ public class BooksController {
         this.bookDAO = bookDAO;
     }
 
-
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("books", bookDAO.index());
+        return "books/index";
+    }
 }
